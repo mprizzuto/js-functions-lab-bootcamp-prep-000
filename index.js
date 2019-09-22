f1167d05b48a66133ebb4126cf03ee48a864d5d3
MARCO RIZZUTO <mprizzuto88@gmail.com>
2019-09-22 19:04:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@
 function happyHolidays() {
   return 'Happy Holidays!'
 }
+
+function happyHolidaysTo(name){
+  return ` Happy holidays ${name}!`
+}
+
+function happyHolidayTo(holiday,name) {
+   return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(days,holiday){
+  return `It's ${days} days until ${holiday}!`
+}
+
